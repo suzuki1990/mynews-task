80e2117b040173fad45f9fd25d19511a0853c89e
”鈴木” <fufukuka160@yahoo.co.jp>
2020-08-14 14:03:12 +0000

    18task

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('admin/profile/create','Admin\ProfileController@add');
+    Route::get('admin/profile/edit','Admin\ProfileController@edit');
+});
+?>
