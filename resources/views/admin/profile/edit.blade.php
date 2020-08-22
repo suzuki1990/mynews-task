7c54c9c902ef8f136f2a9bc2a8967820c359e365
”鈴木” <fufukuka160@yahoo.co.jp>
2020-08-22 14:18:10 +0000

    No,20 task

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -5,9 +5,9 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>MyProfile</title>
+        <title>Myprofileedit</title>
     </head>
     <body>
-        <h1>マイプロフィール作成画面</h1>
+        <h1>マイプロフィール編集画面</h1>
     </body>
 </html>
