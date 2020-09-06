a511a5490f863dcf957062f65b1670702f635a94
”鈴木” <fufukuka160@yahoo.co.jp>
2020-09-06 07:27:47 +0000

    Laravel14 task

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Plofile;
 
 class ProfileController extends Controller
 {
@@ -13,8 +14,19 @@ class ProfileController extends Controller
         return view('admin.profile.create');
     }
 
-    public function create()
+    public function create(Request $request)
     {
+        $this->validate($request, Plofile::$rules);
+        $profile = new Plofile;
+        $form = $request->all();
+    
+     //フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+
+     //データベースに保存
+        $profile->fill($form);
+        $profile->save();
+        
         return redirect('admin/profile/create');
     }
 
