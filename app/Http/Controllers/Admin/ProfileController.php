954e2dd79caf8e71ea67d659e832d96d036f59ae
”鈴木” <fufukuka160@yahoo.co.jp>
2020-09-11 18:13:23 +0000

    PHP/Laravel 16

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -16,8 +16,8 @@ class ProfileController extends Controller
 
     public function create(Request $request)
     {
-        $this->validate($request, Plofile::$rules);
-        $profile = new Plofile;
+        $this->validate($request, Profile::$rules);
+        $profile = new Profile;
         $form = $request->all();
     
      //フォームから送信されてきた_tokenを削除する
@@ -30,13 +30,57 @@ class ProfileController extends Controller
         return redirect('admin/profile/create');
     }
 
-    public function edit()
+    public function index(Request $request)
     {
-        return view('admin.profile.edit');
+      $cond_title = $request->cond_title;
+      if ($cond_title != '') {
+          $posts = Profile::where('title', $cond_title)->get();
+      } else {
+          $posts = Profile::all();
+      }
+      return view('admin.profile.index', ['posts' => $posts, 'cond_title' => $cond_title]);
     }
 
-    public function update()
+    public function edit(Request $request)
     {
-        return redirect('admin/profile/edit');
+        //Profile　Modeleからデータを取得
+        $profile = Profile::find($request->id);
+        if (empty($profile)) {
+          abort(404);
+        }
+        return view('admin.profile.edit', ['profile_form' => $profile]);
     }
+
+    public function update(Request $request)
+    {
+        //validationをかける
+        $this->validate($request,Profile::$rules);
+        //Profile Modeleからデータを取得する
+         $profile = Profile::find($request->id);
+        // 送信されてきたフォームデータを格納する
+        $profile_form = $request->all();
+        if (isset($plofile_form['image'])) {
+        $path = $request->file('image')->store('public/image');
+        $profile->image_path = basename($path);
+        unset($profile_form['image']);
+      } elseif (isset($request->remove)) {
+        $plofile->image_path = null;
+        unset($plofile_form['remove']);
+      }
+        unset($plofile_form['_token']);
+        // 該当するデータを上書きして保存する
+        $plofile->fill($plofile_form)->save();
+
+        return redirect('admin/profile/');
+    }
+    
+    public function delete(Request $request)
+  {
+      // 該当するNews Modelを取得
+      $plofile = Plofile::find($request->id);
+      // 削除する
+      $plofile->delete();
+      return redirect('admin/profile/');
+  }  
+
 }
